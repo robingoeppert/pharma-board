28b2c845330a722dd064610b7d3ff11d55d099c5
robingoeppert <robin.goeppert@gmail.com>
2018-05-01 14:40:25 +0100

    File paths browserified

--- src/App.js
+++ src/App.js
@@ -27,14 +27,14 @@ class App extends Component {
 
         <div className="columns is-8">
           <BoardColumn title="Filtertestgeräte">
-            <Countdown title="A9295" duration={172800000} interval={60000} />
-            <Countdown title="A8500" duration={172800000} interval={60000} />
+            <Countdown title="A9295" duration={172800000} interval={1000} />
+            <Countdown title="A8500" duration={172800000} interval={1000} />
           </BoardColumn>
 
           <BoardColumn title="Infos">
-            <BoardButton title="Schichtplan CD" href="L:\Departements\Pharma\Compounding Department\CD_Allgemein\151 Mitarbeiterinfo\Schichtplan 2018 Compounding.xlsx" />
-            <BoardButton title="Produktionsplan" href="L:\Company\Pharma Planung LPF_PIN_CD\Parenterals Planning_LPF_PIN_CD 2018.xlsx" />
-            <BoardButton title="Staff Meeting" href="L:\Departements\Pharma\Compounding Department\CD_Allgemein\151 Mitarbeiterinfo\Staff Meetings\CD Staff Meetings\Staff Meeting.one" />
+            <BoardButton title="Schichtplan CD" href="file:///L:/Departements/Pharma/Compounding%20Department/CD_Allgemein/151%20Mitarbeiterinfo/Schichtplan%202018%20Compounding.xlsx" />
+            <BoardButton title="Produktionsplan" href="file:///L:/Company/Pharma%20Planung%20LPF_PIN_CD/Parenterals%20Planning_LPF_PIN_CD%202018.xlsx" />
+            <BoardButton title="Staff Meeting" href="file:///L:/Departements/Pharma/Compounding%20Department/CD_Allgemein/151%20Mitarbeiterinfo/Staff%20Meetings/CD%20Staff%20Meetings/Staff%20Meeting.one" />
             <BoardButton title="Netzplan LPF" href="https://prdbispot.jnj.com/spotfire/wp/render/KGOPjF0KXbsD2QPRuf/analysis?file=PHARMA/JSC/OIBI/Manufacturing%20Dashboard/Reports/Manufacturing%20Dashboard&waid=TnIVIUXc_UmAWOFpctoCw-170504a23aaLPC&wavid=0" />
           </BoardColumn>
 
@@ -44,30 +44,20 @@ class App extends Component {
             <BoardButton title="Infoblatt" href="https://docspaceportal.jnj.com/docspaceportal/servlet/OpenPDF?rfrom=null&docname=DS-eFRM-981" />
             <BoardButton title="Waagenkontrollblatt" href="https://docspaceportal.jnj.com/docspaceportal/servlet/OpenPDF?rfrom=null&docname=DS-FRM-18356" />
             <BoardButton title="Schichtbuch Vorlage" href="#" />
-            <BoardButton title="Übergabeprotokoll" href="L:\Departements\Pharma\Compounding Department\CD_Allgemein\übergabeprotokoll-Ansatz.pdf" />
+            <BoardButton title="Übergabeprotokoll" href="file:///L:/Departements/Pharma/Compounding%20Department/CD_Allgemein/%C3%BCbergabeprotokoll-Ansatz.pdf" />
           </BoardColumn>
 
           <BoardColumn title="Sonstiges">
             <BoardButton title="CILAG Telefon Link" href="http://browsejjeds.jnj.com/" />
             <BoardButton title="Cilag WLAN Passwort" href="http://guestpass.jnj.com/" />
-            <BoardButton title="Doc Space Link" href="L:\Departements\Pharma\Compounding Department\CD_Allgemein\Dashboard\Doc Space Link.xlsm" />
+            <BoardButton title="Doc Space Link" href="file:///L:/Departements/Pharma/Compounding%20Department/CD_Allgemein/Dashboard/Doc%20Space%20Link.xlsm" />
             <BoardButton title="Abteilungs Web" href="https://jnj.sharepoint.com/sites/PHM-GCSP-JSC/campuses/Schaffhausen/Abteilungswebs/Parenterals%20CD/Seiten/default.aspx" />
-            <BoardButton title="Auslagerungs- und Musterliste" href="L:\Departements\Pharma\Compounding Department\CD_Allgemein\614 Planung\2018\Auslagerungs- und Musterliste_CD2018.xlsx" />
+            <BoardButton title="Auslagerungs- &amp; Musterliste" href="file:///L:/Departements/Pharma/Compounding%20Department/CD_Allgemein/614%20Planung/2018/Auslagerungs-%20und%20Musterliste_CD2018.xlsx" />
             <BoardButton title="Kessel Übersicht" href="#" />
-            <BoardButton title="Brutto-Tara Erfassung" href="L:\Departements\Pharma\Compounding Department\CD_Allgemein\Lyo Compounding\INFO_Batchvorbereitung-Comp\Brutto-Tara erfassung" />
+            <BoardButton title="Brutto-Tara Erfassung" href="file:///L:/Departements/Pharma/Compounding%20Department/CD_Allgemein/Lyo%20Compounding/INFO_Batchvorbereitung-Comp/Brutto-Tara%20erfassung" />
           </BoardColumn>
         </div>
       </div>
-      /*
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>*/
     );
   }
 }
